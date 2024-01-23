5afb5be2f932a6a178a7797e1f794d613178b202
manumathani <manumathani02@gmail.com>
2024-01-23 17:09:23 +0300

    Update result action creator

--- src/hooks/setResult.js
+++ src/hooks/setResult.js
@@ -6,4 +6,12 @@ export const pushAnswer = (result) => async (dispatch) => {
     } catch (error) {
         console.log(error)
     }
+};
+
+export const updateResult = (index) => async(dispatch) => {
+    try {
+        await dispatch(Action.updateResultAction(index))
+    } catch (error) {
+        
+    }
 }
